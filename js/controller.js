34ea1bcba08bc2e4d511376c0d817b9cf81a914b
foojack87 <foojack@gmail.com>
2022-12-04 09:40:53 +0800

    building UI for displaying crypto coin data

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,21 @@
-// https://api.coingecko.com/api/v3/coins/bitcoin?community_data=false&developer_data=false&sparkline=true
+// https://api.coingecko.com/api/v3/coins/bitcoin?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true
+
+'use strict';
+
+const showCoinData = async function () {
+  try {
+    const res = await fetch(
+      'https://api.coingecko.com/api/v3/coins/bitcoin?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true'
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.error} ${res.status}`);
+
+    console.log(res, data);
+    // console.log(data.);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showCoinData();
