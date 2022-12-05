a187064bee1a80b7c963128a9738f3db14358ac9
foojack87 <foojack@gmail.com>
2022-12-05 22:26:39 +0800

    added top 10 coins ranking

--- js/controller.js
+++ js/controller.js
@@ -30,6 +30,14 @@ const showCoinData = async function () {
     await model.loadTop10CoinData();
     const btc = model.state.btc;
     const eth = model.state.eth;
+    const tether = model.state.tether;
+    const bnb = model.state.bnb;
+    const usdc = model.state.usdc;
+    const busd = model.state.busd;
+    const xrp = model.state.xrp;
+    const doge = model.state.doge;
+    const cardano = model.state.cardano;
+    const matic = model.state.matic;
 
     // Rendering coin data
 
@@ -84,6 +92,203 @@ const showCoinData = async function () {
   <td class="7d-sparkline center-text sparkline><canvas id="sparkline-chart" width="100" height="50"></canvas></td>
 </tr>
 
+<tr class="border-top">
+
+<td class="first-col coin-number center-text ">${tether.mktCapRank}</td>
+<td class="second-col coin-name center-text"><img src="${
+      tether.logo
+    }" class="coin-logos"><span class="font-bold">${
+      tether.name
+    }</span><span class="coin-symbol">${tether.symbol.toUpperCase()}</span></td>
+<td class="coin-price center-text">${formatterPrice.format(
+      tether.currUsdPrice
+    )}</td>
+<td class="1hr-change center-text"><span class="text-negative">
+${tether.priceChangePercent1h.toFixed(2)}%</span></td>
+<td class="24hr-change center-text"><span class="text-positive">${tether.priceChangePercent24h.toFixed(
+      2
+    )}%</span></td>
+<td class="7d-change center-text"><span class="text-positive">${tether.priceChangePercent7d.toFixed(
+      2
+    )}%</span></td>
+<td class="24hr-vol center-text">${formatter.format(
+      tether.totalVolume24hr
+    )}</td>
+<td class="mkt-cap center-text">${formatter.format(tether.mktCap)}</td>
+<td class="7d-sparkline center-text sparkline><canvas id="sparkline-chart" width="100" height="50"></canvas></td>
+</tr>
+
+<tr class="border-top">
+
+<td class="first-col coin-number center-text ">${bnb.mktCapRank}</td>
+<td class="second-col coin-name center-text"><img src="${
+      bnb.logo
+    }" class="coin-logos"><span class="font-bold">${
+      bnb.name
+    }</span><span class="coin-symbol">${bnb.symbol.toUpperCase()}</span></td>
+<td class="coin-price center-text">${formatterPrice.format(
+      bnb.currUsdPrice
+    )}</td>
+<td class="1hr-change center-text"><span class="text-negative">
+${bnb.priceChangePercent1h.toFixed(2)}%</span></td>
+<td class="24hr-change center-text"><span class="text-positive">${bnb.priceChangePercent24h.toFixed(
+      2
+    )}%</span></td>
+<td class="7d-change center-text"><span class="text-positive">${bnb.priceChangePercent7d.toFixed(
+      2
+    )}%</span></td>
+<td class="24hr-vol center-text">${formatter.format(bnb.totalVolume24hr)}</td>
+<td class="mkt-cap center-text">${formatter.format(bnb.mktCap)}</td>
+<td class="7d-sparkline center-text sparkline><canvas id="sparkline-chart" width="100" height="50"></canvas></td>
+</tr>
+
+
+<tr class="border-top">
+
+<td class="first-col coin-number center-text ">${usdc.mktCapRank}</td>
+<td class="second-col coin-name center-text"><img src="${
+      usdc.logo
+    }" class="coin-logos"><span class="font-bold">${
+      usdc.name
+    }</span><span class="coin-symbol">${usdc.symbol.toUpperCase()}</span></td>
+<td class="coin-price center-text">${formatterPrice.format(
+      usdc.currUsdPrice
+    )}</td>
+<td class="1hr-change center-text"><span class="text-negative">
+${usdc.priceChangePercent1h.toFixed(2)}%</span></td>
+<td class="24hr-change center-text"><span class="text-positive">${usdc.priceChangePercent24h.toFixed(
+      2
+    )}%</span></td>
+<td class="7d-change center-text"><span class="text-positive">${usdc.priceChangePercent7d.toFixed(
+      2
+    )}%</span></td>
+<td class="24hr-vol center-text">${formatter.format(usdc.totalVolume24hr)}</td>
+<td class="mkt-cap center-text">${formatter.format(usdc.mktCap)}</td>
+<td class="7d-sparkline center-text sparkline><canvas id="sparkline-chart" width="100" height="50"></canvas></td>
+</tr>
+
+<tr class="border-top">
+
+<td class="first-col coin-number center-text ">${busd.mktCapRank}</td>
+<td class="second-col coin-name center-text"><img src="${
+      busd.logo
+    }" class="coin-logos"><span class="font-bold">${
+      busd.name
+    }</span><span class="coin-symbol">${busd.symbol.toUpperCase()}</span></td>
+<td class="coin-price center-text">${formatterPrice.format(
+      busd.currUsdPrice
+    )}</td>
+<td class="1hr-change center-text"><span class="text-negative">
+${busd.priceChangePercent1h.toFixed(2)}%</span></td>
+<td class="24hr-change center-text"><span class="text-positive">${busd.priceChangePercent24h.toFixed(
+      2
+    )}%</span></td>
+<td class="7d-change center-text"><span class="text-positive">${busd.priceChangePercent7d.toFixed(
+      2
+    )}%</span></td>
+<td class="24hr-vol center-text">${formatter.format(busd.totalVolume24hr)}</td>
+<td class="mkt-cap center-text">${formatter.format(busd.mktCap)}</td>
+<td class="7d-sparkline center-text sparkline><canvas id="sparkline-chart" width="100" height="50"></canvas></td>
+</tr>
+
+<tr class="border-top">
+
+<td class="first-col coin-number center-text ">${xrp.mktCapRank}</td>
+<td class="second-col coin-name center-text"><img src="${
+      xrp.logo
+    }" class="coin-logos"><span class="font-bold">${
+      xrp.name
+    }</span><span class="coin-symbol">${xrp.symbol.toUpperCase()}</span></td>
+<td class="coin-price center-text">${formatterPrice.format(
+      xrp.currUsdPrice
+    )}</td>
+<td class="1hr-change center-text"><span class="text-negative">
+${xrp.priceChangePercent1h.toFixed(2)}%</span></td>
+<td class="24hr-change center-text"><span class="text-positive">${xrp.priceChangePercent24h.toFixed(
+      2
+    )}%</span></td>
+<td class="7d-change center-text"><span class="text-positive">${xrp.priceChangePercent7d.toFixed(
+      2
+    )}%</span></td>
+<td class="24hr-vol center-text">${formatter.format(xrp.totalVolume24hr)}</td>
+<td class="mkt-cap center-text">${formatter.format(xrp.mktCap)}</td>
+<td class="7d-sparkline center-text sparkline><canvas id="sparkline-chart" width="100" height="50"></canvas></td>
+</tr>
+
+<tr class="border-top">
+
+<td class="first-col coin-number center-text ">${doge.mktCapRank}</td>
+<td class="second-col coin-name center-text"><img src="${
+      doge.logo
+    }" class="coin-logos"><span class="font-bold">${
+      doge.name
+    }</span><span class="coin-symbol">${doge.symbol.toUpperCase()}</span></td>
+<td class="coin-price center-text">${formatterPrice.format(
+      doge.currUsdPrice
+    )}</td>
+<td class="1hr-change center-text"><span class="text-negative">
+${doge.priceChangePercent1h.toFixed(2)}%</span></td>
+<td class="24hr-change center-text"><span class="text-positive">${doge.priceChangePercent24h.toFixed(
+      2
+    )}%</span></td>
+<td class="7d-change center-text"><span class="text-positive">${doge.priceChangePercent7d.toFixed(
+      2
+    )}%</span></td>
+<td class="24hr-vol center-text">${formatter.format(doge.totalVolume24hr)}</td>
+<td class="mkt-cap center-text">${formatter.format(doge.mktCap)}</td>
+<td class="7d-sparkline center-text sparkline><canvas id="sparkline-chart" width="100" height="50"></canvas></td>
+</tr>
+
+<tr class="border-top">
+
+<td class="first-col coin-number center-text ">${cardano.mktCapRank}</td>
+<td class="second-col coin-name center-text"><img src="${
+      cardano.logo
+    }" class="coin-logos"><span class="font-bold">${
+      cardano.name
+    }</span><span class="coin-symbol">${cardano.symbol.toUpperCase()}</span></td>
+<td class="coin-price center-text">${formatterPrice.format(
+      cardano.currUsdPrice
+    )}</td>
+<td class="1hr-change center-text"><span class="text-negative">
+${cardano.priceChangePercent1h.toFixed(2)}%</span></td>
+<td class="24hr-change center-text"><span class="text-positive">${cardano.priceChangePercent24h.toFixed(
+      2
+    )}%</span></td>
+<td class="7d-change center-text"><span class="text-positive">${cardano.priceChangePercent7d.toFixed(
+      2
+    )}%</span></td>
+<td class="24hr-vol center-text">${formatter.format(
+      cardano.totalVolume24hr
+    )}</td>
+<td class="mkt-cap center-text">${formatter.format(cardano.mktCap)}</td>
+<td class="7d-sparkline center-text sparkline><canvas id="sparkline-chart" width="100" height="50"></canvas></td>
+</tr>
+
+<tr class="border-top">
+
+<td class="first-col coin-number center-text ">${matic.mktCapRank}</td>
+<td class="second-col coin-name center-text"><img src="${
+      matic.logo
+    }" class="coin-logos"><span class="font-bold">${
+      matic.name
+    }</span><span class="coin-symbol">${matic.symbol.toUpperCase()}</span></td>
+<td class="coin-price center-text">${formatterPrice.format(
+      matic.currUsdPrice
+    )}</td>
+<td class="1hr-change center-text"><span class="text-negative">
+${matic.priceChangePercent1h.toFixed(2)}%</span></td>
+<td class="24hr-change center-text"><span class="text-positive">${matic.priceChangePercent24h.toFixed(
+      2
+    )}%</span></td>
+<td class="7d-change center-text"><span class="text-positive">${matic.priceChangePercent7d.toFixed(
+      2
+    )}%</span></td>
+<td class="24hr-vol center-text">${formatter.format(matic.totalVolume24hr)}</td>
+<td class="mkt-cap center-text">${formatter.format(matic.mktCap)}</td>
+<td class="7d-sparkline center-text sparkline><canvas id="sparkline-chart" width="100" height="50"></canvas></td>
+</tr>
+
     `;
     // coinDataContainer.innerHTML = '';
     coinDataContainer.insertAdjacentHTML('beforeend', markup);
@@ -93,8 +298,3 @@ const showCoinData = async function () {
 };
 
 showCoinData();
-
-// showCoinData('ethereum');
-// showCoinData('tether');
-// showCoinData('binancecoin');
-// showCoinData('usd-coin');
--- js/model.js
+++ js/model.js
@@ -4,6 +4,8 @@ export const state = {
   eth: {},
   tether: {},
   bnb: {},
+  usdc: {},
+  busd: {},
   xrp: {},
   doge: {},
   cardano: {},
@@ -38,8 +40,6 @@ export const loadCoinData = async function (id) {
       sparkline: data.market_data.sparkline_7d,
       mktCapRank: data.market_cap_rank,
     };
-
-    console.log(state.coins);
   } catch (err) {
     alert(err);
   }
@@ -58,27 +58,27 @@ export const loadTop10CoinData = async function () {
     fetch(
       `https://api.coingecko.com/api/v3/coins/tether?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true`
     ).then(res => res.json()),
-    // fetch(
-    //   `https://api.coingecko.com/api/v3/coins/binancecoin?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true`
-    // ).then(res => res.json()),
-    // fetch(
-    //   `https://api.coingecko.com/api/v3/coins/usd-coin?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true`
-    // ).then(res => res.json()),
-    // fetch(
-    //   `https://api.coingecko.com/api/v3/coins/binance-usd?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true`
-    // ).then(res => res.json()),
-    // fetch(
-    //   `https://api.coingecko.com/api/v3/coins/ripple?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true`
-    // ).then(res => res.json()),
-    // fetch(
-    //   `https://api.coingecko.com/api/v3/coins/dogecoin?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true`
-    // ).then(res => res.json()),
-    // fetch(
-    //   `https://api.coingecko.com/api/v3/coins/cardano?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true`
-    // ).then(res => res.json()),
-    // fetch(
-    //   `https://api.coingecko.com/api/v3/coins/matic-network?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true`
-    // ).then(res => res.json()),
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/binancecoin?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true`
+    ).then(res => res.json()),
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/usd-coin?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true`
+    ).then(res => res.json()),
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/binance-usd?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true`
+    ).then(res => res.json()),
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/ripple?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true`
+    ).then(res => res.json()),
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/dogecoin?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true`
+    ).then(res => res.json()),
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/cardano?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true`
+    ).then(res => res.json()),
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/matic-network?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true`
+    ).then(res => res.json()),
   ]);
 
   console.log(data);
@@ -126,7 +126,165 @@ export const loadTop10CoinData = async function () {
       mktCapRank: data[1].market_cap_rank,
     };
 
-    console.log(state.eth);
+    const { tether } = data;
+
+    state.tether = {
+      id: data[2].id,
+      symbol: data[2].symbol,
+      name: data[2].name,
+      logo: data[2].image.thumb,
+      currUsdPrice: data[2].market_data.current_price.usd,
+      mktCap: data[2].market_data.market_cap.usd,
+      priceChangePercent1h:
+        data[2].market_data.price_change_percentage_1h_in_currency.usd,
+      priceChangePercent24h:
+        data[2].market_data.price_change_percentage_24h_in_currency.usd,
+      priceChangePercent7d:
+        data[2].market_data.price_change_percentage_7d_in_currency.usd,
+      totalVolume24hr: data[2].market_data.total_volume.usd,
+      sparkline: data[2].market_data.sparkline_7d,
+      mktCapRank: data[2].market_cap_rank,
+    };
+
+    const { bnb } = data;
+
+    state.bnb = {
+      id: data[3].id,
+      symbol: data[3].symbol,
+      name: data[3].name,
+      logo: data[3].image.thumb,
+      currUsdPrice: data[3].market_data.current_price.usd,
+      mktCap: data[3].market_data.market_cap.usd,
+      priceChangePercent1h:
+        data[3].market_data.price_change_percentage_1h_in_currency.usd,
+      priceChangePercent24h:
+        data[3].market_data.price_change_percentage_24h_in_currency.usd,
+      priceChangePercent7d:
+        data[3].market_data.price_change_percentage_7d_in_currency.usd,
+      totalVolume24hr: data[3].market_data.total_volume.usd,
+      sparkline: data[3].market_data.sparkline_7d,
+      mktCapRank: data[3].market_cap_rank,
+    };
+
+    const { usdc } = data;
+
+    state.usdc = {
+      id: data[4].id,
+      symbol: data[4].symbol,
+      name: data[4].name,
+      logo: data[4].image.thumb,
+      currUsdPrice: data[4].market_data.current_price.usd,
+      mktCap: data[4].market_data.market_cap.usd,
+      priceChangePercent1h:
+        data[4].market_data.price_change_percentage_1h_in_currency.usd,
+      priceChangePercent24h:
+        data[4].market_data.price_change_percentage_24h_in_currency.usd,
+      priceChangePercent7d:
+        data[4].market_data.price_change_percentage_7d_in_currency.usd,
+      totalVolume24hr: data[4].market_data.total_volume.usd,
+      sparkline: data[4].market_data.sparkline_7d,
+      mktCapRank: data[4].market_cap_rank,
+    };
+
+    const { busd } = data;
+
+    state.busd = {
+      id: data[5].id,
+      symbol: data[5].symbol,
+      name: data[5].name,
+      logo: data[5].image.thumb,
+      currUsdPrice: data[5].market_data.current_price.usd,
+      mktCap: data[5].market_data.market_cap.usd,
+      priceChangePercent1h:
+        data[5].market_data.price_change_percentage_1h_in_currency.usd,
+      priceChangePercent24h:
+        data[5].market_data.price_change_percentage_24h_in_currency.usd,
+      priceChangePercent7d:
+        data[5].market_data.price_change_percentage_7d_in_currency.usd,
+      totalVolume24hr: data[5].market_data.total_volume.usd,
+      sparkline: data[5].market_data.sparkline_7d,
+      mktCapRank: data[5].market_cap_rank,
+    };
+
+    const { xrp } = data;
+
+    state.xrp = {
+      id: data[6].id,
+      symbol: data[6].symbol,
+      name: data[6].name,
+      logo: data[6].image.thumb,
+      currUsdPrice: data[6].market_data.current_price.usd,
+      mktCap: data[6].market_data.market_cap.usd,
+      priceChangePercent1h:
+        data[6].market_data.price_change_percentage_1h_in_currency.usd,
+      priceChangePercent24h:
+        data[6].market_data.price_change_percentage_24h_in_currency.usd,
+      priceChangePercent7d:
+        data[6].market_data.price_change_percentage_7d_in_currency.usd,
+      totalVolume24hr: data[6].market_data.total_volume.usd,
+      sparkline: data[6].market_data.sparkline_7d,
+      mktCapRank: data[6].market_cap_rank,
+    };
+
+    const { doge } = data;
+
+    state.doge = {
+      id: data[7].id,
+      symbol: data[7].symbol,
+      name: data[7].name,
+      logo: data[7].image.thumb,
+      currUsdPrice: data[7].market_data.current_price.usd,
+      mktCap: data[7].market_data.market_cap.usd,
+      priceChangePercent1h:
+        data[7].market_data.price_change_percentage_1h_in_currency.usd,
+      priceChangePercent24h:
+        data[7].market_data.price_change_percentage_24h_in_currency.usd,
+      priceChangePercent7d:
+        data[7].market_data.price_change_percentage_7d_in_currency.usd,
+      totalVolume24hr: data[7].market_data.total_volume.usd,
+      sparkline: data[7].market_data.sparkline_7d,
+      mktCapRank: data[7].market_cap_rank,
+    };
+
+    const { cardano } = data;
+
+    state.cardano = {
+      id: data[8].id,
+      symbol: data[8].symbol,
+      name: data[8].name,
+      logo: data[8].image.thumb,
+      currUsdPrice: data[8].market_data.current_price.usd,
+      mktCap: data[8].market_data.market_cap.usd,
+      priceChangePercent1h:
+        data[8].market_data.price_change_percentage_1h_in_currency.usd,
+      priceChangePercent24h:
+        data[8].market_data.price_change_percentage_24h_in_currency.usd,
+      priceChangePercent7d:
+        data[8].market_data.price_change_percentage_7d_in_currency.usd,
+      totalVolume24hr: data[8].market_data.total_volume.usd,
+      sparkline: data[8].market_data.sparkline_7d,
+      mktCapRank: data[8].market_cap_rank,
+    };
+
+    const { matic } = data;
+
+    state.matic = {
+      id: data[9].id,
+      symbol: data[9].symbol,
+      name: data[9].name,
+      logo: data[9].image.thumb,
+      currUsdPrice: data[9].market_data.current_price.usd,
+      mktCap: data[9].market_data.market_cap.usd,
+      priceChangePercent1h:
+        data[9].market_data.price_change_percentage_1h_in_currency.usd,
+      priceChangePercent24h:
+        data[9].market_data.price_change_percentage_24h_in_currency.usd,
+      priceChangePercent7d:
+        data[9].market_data.price_change_percentage_7d_in_currency.usd,
+      totalVolume24hr: data[9].market_data.total_volume.usd,
+      sparkline: data[9].market_data.sparkline_7d,
+      mktCapRank: data[9].market_cap_rank,
+    };
   } catch (err) {
     alert(err);
   }
